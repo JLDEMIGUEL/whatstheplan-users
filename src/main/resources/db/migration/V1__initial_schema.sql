d37b37abf36dbcd14647b7a1afde8fa80ccb068e
jldemiguel <jldmp2001@gmail.com>
2025-01-19 23:01:26 +0100

    Make username unique ignore case

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,7 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+
 CREATE TABLE users
 (
     id                 UUID PRIMARY KEY,
-    username           VARCHAR(255) NOT NULL UNIQUE,
+    username           CITEXT NOT NULL UNIQUE,
     email              VARCHAR(255) NOT NULL UNIQUE,
     first_name         VARCHAR(255),
     last_name          VARCHAR(255),
@@ -10,16 +12,14 @@ CREATE TABLE users
     last_modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
 CREATE TABLE preferences
 (
     id                 UUID PRIMARY KEY,
     activity_type      VARCHAR(255) NOT NULL,
-    user_id            UUID         NOT NULL,
+    user_id            UUID NOT NULL,
     created_date       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_preferences_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
-
 
 CREATE INDEX idx_preferences_user_id ON preferences (user_id);
